89372355dee6cd15323876f2f24251b12fa5384f
Cong Zhang <congzhangzh@gmail.com>
2018-06-11 02:53:45 +0800

    alpha status

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='oum',
-    version='0.09',
+    version='0.99',
     packages=['oum'],
     url='https://github.com/medlab/oum',
     license='BSD 3-Clause "New" or "Revised" License',
