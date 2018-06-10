1808fea8b846d69104ff7005a3d2069973e30dca
Cong Zhang <congzhangzh@gmail.com>
2018-06-10 22:00:19 +0800

    basic self QA idea here

---
+++ tests/hello_test.py
@@ -0,0 +1,69 @@
+import unittest
+
+from ..uiapps import uia_contract_for_test
+
+class BasicSeleniumElementRetrieveAndPropertyAcessTestCase(unittest.TestCase):
+    def test_get_one_by_id(self):
+        pass
+
+    def test_property_access(self):
+        pass
+
+    def test_get_many_by_id(self):
+        pass
+
+    def test_get_one_by_name(self):
+        pass
+
+    def test_get_many_by_name(self):
+        pass
+
+    def test_get_one_by_xpath(self):
+        pass
+
+    def test_get_many_by_xpath(self):
+        pass
+
+    pass
+
+
+class BasicOUMDriveElementRetrieveAndPropertyAcessTestCase(unittest.TestCase):
+    def test_element_mark_by_id(self):
+        pass
+
+    def test_property_access(self):
+        pass
+
+    def test_elements_mark_by_id(self):
+        pass
+
+    def test_element_mark_by_name(self):
+        pass
+
+    def test_elements_mark__by_name(self):
+        pass
+
+    def test_element_mark_by_xpath(self):
+        pass
+
+    def test_elements_mark_by_xpath(self):
+        pass
+    pass
+
+
+class AdvanceOUMDriveElementRetrieveAndPropertyAcessTestCase(unittest.TestCase):
+    def test_lazy_element_mark_by_id(self):
+        pass
+
+    def test_property_access(self):
+        pass
+
+    def test_dirty_element_mark_by_id(self):
+        pass
+
+    def test_dynamic_and_dirty_subtree_elements_mark_by_id_(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
